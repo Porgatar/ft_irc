94e0bcf8940d0bf692c86208fd147801b507fb75
maxime <maximedesroses96@gmail.com>
2024-04-30 21:42:13 +0200

    Added: kick function, for now, it can be used if you are registered but not in channel and not in operator

--- srcs/Channel/UserManagement.cpp
+++ srcs/Channel/UserManagement.cpp
@@ -3,28 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   UserManagement.cpp                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: parinder <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: maxime <maxime@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 00:05:32 by parinder          #+#    #+#             */
-/*   Updated: 2024/04/30 00:12:54 by parinder         ###   ########.fr       */
+/*   Updated: 2024/04/30 21:39:44 by maxime           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/Channel.hpp"
 
-bool	Channel::isConnected(User &user) {
+bool	Channel::isConnected(std::string nick) {
 	
 	std::list<User>::iterator it;
 
 	for (it = _users.begin(); it != _users.end(); it++) {
-
-		if (user.getNickname().compare(it->getNickname().c_str()) == 0) {
-
-			write(user.getSocket(), "User already connected\n", 23);	
+		if (nick.compare(it->getNickname().c_str()) == 0)
 			return (true);
-		}
 	}
 	return (false);
+}
+
+void	Channel::kickuser(std::string nick, std::string message) {
+	
+	std::list<User>::iterator it;
+	
+	for (it = _users.begin(); it != _users.end(); it++) {
+		if (nick.compare(it->getNickname().c_str()) == 0) {
+			this->sendGroupMsg(it->getNickname() + " " + message + "\n");
+			_users.erase(it);
+			return ;
+		}
+	}
 }
 
 void	Channel::sendGroupMsg(std::string msg) {
