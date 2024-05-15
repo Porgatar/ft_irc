ee2c74ada75329b37b47b22a75a674f9f856f9b2
Patrick Rinderspacher <parinder@2C4.42angouleme.fr>
2024-05-15 21:48:18 +0200

    Channel
    -	mode command added k and l argument.
    -	removed some modes and added key, userLimit to channel class variables.
    -	corrected ctrl-c print message in main.

--- headers/Channel.hpp
+++ headers/Channel.hpp
@@ -6,7 +6,7 @@
 /*   By: mdesrose <mdesrose@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 23:38:59 by parinder          #+#    #+#             */
-/*   Updated: 2024/05/14 16:04:37 by parinder         ###   ########.fr       */
+/*   Updated: 2024/05/15 18:33:23 by parinder         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,12 @@ class Channel {
 private:
 
 	std::string		_name;
+	std::string		_key;
 	std::string		_topic;
-	bool			_modes[4];
+	bool			_modes[2];
 	std::list<User>	_users[3];
-	
+	size_t			_userLimit;
+
 public:
 
 	Channel(void);
@@ -34,13 +36,17 @@ public:
 	Channel	&operator=(const Channel &rhs);
 	
 	const std::string	&getName(void) const ;
+	const std::string	&getKey(void) const ;
 	const std::string	&getTopic(void) const ;
 	bool				getMode(const int &mode) const ;
 	User				*getUserByNameFrom(const int &list, const std::string &nick);
+	const size_t		&getUserLimit(void) const ;
 
+	void	setKey(const std::string &key);
 	void	setTopic(const std::string &name);
 	void	setMode(const int &mode, const bool &state);
 	void	addUserTo(const int &list, const User &user);
+	void	setUserLimit(const size_t &userLimit);
 
 	void	sendGroupMsg(const std::string &msg);
 	void	removeUserByNameFrom(const int &list, const std::string &nick);
