66dadc35940f7d2bc0d72b3c66832d6b224e5ae3
maxime <maximedesroses96@gmail.com>
2024-06-27 20:31:57 +0200

    rien d'important

--- srcs/server/cmd/invite.cpp
+++ srcs/server/cmd/invite.cpp
@@ -4,6 +4,7 @@
 void    Irc::invite(User &actual)
 {
     std::list<User>::iterator it;
+    std::list<Channel>::iterator chan;
 
     if (_args.size() < 3)
         return; //a changer
@@ -11,14 +12,18 @@ void    Irc::invite(User &actual)
         return;
     else if (checkExistingChannel(_args[2]) == false)
         return;
+    chan = getChannelIteratorByName(_args[2]);
     //si ce user est sur le channel ou il souhaite inviter quelquun
-    else if (getChannelIteratorByName(_args[2])->isIn(USER_LIST, actual.getNickname()) == false)
-        return;
-    else if (getChannelIteratorByName(_args[2])->isIn(USER_LIST, _args[1]) == true)
-        return;
+    if (chan->isIn(USER_LIST, actual.getNickname()) == false)
+        return; // n'est pas dans le canal
+    else if (chan->isIn(OPERATOR_LIST, actual.getNickname()) == false)
+        return; // nest pas operateur du canal
+    else if (chan->isIn(USER_LIST, _args[1]) == true)
+        return; // si il est deja dans le canal
     for (it = _users.begin(); it != _users.end(); it++) {
         if (_args[1] == it->getNickname())
             break;
     }
-    getChannelIteratorByName(_args[2])->addUserTo(INVITE_LIST, *it);
+    chan->addUserTo(INVITE_LIST, *it);
+    actual.sendMsg(it->getNickname() + " has been invited to channel : " + _args[2]);
 }
