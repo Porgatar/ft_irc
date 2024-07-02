366a73c985a56cbb53c0c27f467515290ce77b3b
Patrick Rinderspacher <parinder@student.42angouleme.fr>
2024-07-02 17:47:50 +0200

    reply in all commands
    -	added replys (and 'this->' token) tosome commandse
    -	still needs to do nick, privMsg topic, user

--- headers/header.h
+++ headers/header.h
@@ -6,7 +6,7 @@
 /*   By: maxime <maxime@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 19:28:31 by parinder          #+#    #+#             */
-/*   Updated: 2024/07/01 18:52:42 by parinder         ###   ########.fr       */
+/*   Updated: 2024/07/02 17:41:51 by parinder         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,67 +70,86 @@
 
 /*	-	-	-	-	-	Reply Macros	-	-	-	-	-	*/
 
-#define MODE(client, channelName, mode)	\
-	client, INFO, std::string(":") + client.getNickname() + " " + channelName \
+#define MODE(client, channel, mode)	\
+	client, INFO, std::string(":") + client.getNickname() + " " + channel \
 	+ " " + mode
+
+#define UNKNOWNCOMMAND(client) \
+	client, WARNING, std::string(":") + client.getNickname() + " :Unkown command"
 
 #define WELCOME(client) \
 	client, INFO, std::string(": 001 ") + client.getNickname() \
 	+ " :Welcome to The Internet Relay Chat world, " + client.getNickname()
 
+#define USERONCHAN(client, nick, channel) \
+	client, WARNING, std::string(": 303 ") + client.getNickname() + " " + nick \
+	+ " " +  channel + " :is already on channel"
+
 #define NOTOPIC(client, channel) \
 	client, INFO, std::string(": 331 ") + client.getNickname() + " " + channel \
 	+ " :No topic is set"
 
-#define NOTREGISTERED(client) \
-	client, WARNING, std::string(": 451 ") + client.getNickname() \
-	+ " :You have not registered"
-
-#define ALREADYREGISTERED(client) \
-	client, WARNING, std::string(": 462 ") + client.getNickname() \
-	+ " :You may not reregister"
-
-#define UNKNOWNCOMMAND(client) \
-	client, WARNING, std::string(":") + client.getNickname() + " :Unkown command"
-
-#define NOSUCHCHANNEL(client, channel) \
-	client, WARNING, std::string(": 403 ") + client.getNickname() + " " + channel \
-	+ " :No such channel"
-
-#define CHANOPRIVSNEEDED(client, channel) \
-	client, WARNING, std::string(": 482 ") + client.getNickname() + " " + channel \
-	+ " :You're not channel operator"
-
-#define UNKNOWNMODE(client, mode) \
-	client, WARNING, std::string(": 472 ") + client.getNickname() + " " + mode \
-	+ " :Is unknown mode char to me"
+#define INVITE(client, invitee, channel) \
+	client, INFO, ": 341 " + client.getNickname() + " " + invitee + " " + channel
 
 #define NOSUCHNICK(client, nickname) \
 	client, WARNING, std::string(": 401 ") + client.getNickname() + " " + nickname \
 	+ " :No such nickname"
 
-#define NOTONCHANNEL(client, channel) \
+#define NOSUCHCHAN(client, channel) \
+	client, WARNING, std::string(": 403 ") + client.getNickname() + " " + channel \
+	+ " :No such channel"
+
+#define USERNOTINCHAN(client, nick, channel) \
+	client, WARNING, std::string(": 441 ") + client.getNickname() + " " + nick + " " \
+	+ channel + " :They aren't on that channel"
+
+#define NOTONCHAN(client, channel) \
 	client, WARNING, std::string(": 442 ") + client.getNickname() + " " + channel \
 	+ " :Not on that channel"
 
-#define CHANOPRIVSNEEDED(client, channel) \
-	client, WARNING, std::string(": 482 ") + client.getNickname() + " " + channel \
-	+ " :You're not channel operator"
-
-#define USERONCHANNEL(client, nick, channel) \
-	client, WARNING, std::string(": 303 ") + client.getNickname() + " " + nick \
-	+ " " +  channel + " :is already on channel"
+#define NOTREGISTERED(client) \
+	client, WARNING, std::string(": 451 ") + client.getNickname() \
+	+ " :You have not registered"
 
 #define NEEDMOREPARAMS(client, cmd) \
 	client, WARNING, std::string(": 461 ") + client.getNickname() + " " + cmd \
 	+ " :Not enough parameters"
 
+#define ALREADYREGISTERED(client) \
+	client, WARNING, std::string(": 462 ") + client.getNickname() \
+	+ " :You may not reregister"
+
 #define PASSWDMISMATCH(client) \
 	client, WARNING, std::string(": 464 ") + client.getNickname() \
 	+ " :Password incorrect"
 
+#define CHANISFULL(client, channel) \
+	client, WARNING, std::string(": 471 ") + client.getNickname() + " " + channel \
+	+ " :Cannot join channel (+l)"
+
+#define INVITEONLYCHAN(client, channel) \
+	client, WARNING, std::string(": 473 ") + client.getNickname() + " " + channel \
+	+ " :Cannot join channel (+i)"
+
+#define UNKNOWNMODE(client, mode) \
+	client, WARNING, std::string(": 472 ") + client.getNickname() + " " + mode \
+	+ " :Is unknown mode char to me"
+
+#define BADCHANKEY(client, channel) \
+	client, WARNING, std::string(": 475 ") + client.getNickname() + " " + channel \
+	+ " :Cannot join channel (+k)"
+
+#define BADCHANMASK(client, channel) \
+	client, WARNING, std::string(": 476 ") + client.getNickname() + channel \
+	+ " :Bad Channel Mask"
+
+#define CHANOPRIVSNEEDED(client, channel) \
+	client, WARNING, std::string(": 482 ") + client.getNickname() + " " + channel \
+	+ " :You're not channel operator"
+
 /*	momontanly unused Macros
-#define JOIN_RPL(nick, channel)                        (":" + nick + " JOIN " + channel)
+
 #define PART_RPL(client, channel)                    (":" + client + " PART " + channel)
 #define KICK_RPL(client, channel, target)            (":" + client + " KICK " + channel + " " + target)
 #define NICK_RPL(oldNick, newNick)                    (":" + oldNick + " NICK " + newNick)
@@ -138,7 +157,7 @@
 #define TOPIC_RPL(client, channel, topic)            (":" + client + " TOPIC " + channel + " :" + topic)
 
 #define SEETOPIC_RPL(client, channel, topic)        (": 332 " + client + " " + channel + " :" + topic)
-#define INVITESNDR_RPL(client, invitee, channel)    (": 341 " + client + " " + invitee + " " + channel)
+
 #define NAMEREPLY_RPL(nick, channel, nicknames)        (": 353 " + nick + " = " + channel + " :" + nicknames)
 
 #define TOOMUCHPARAMS_ERR(client, cmd)                (client + " " + cmd + " :Too much parameters")
@@ -148,12 +167,12 @@
 #define NONICKNAMEGIVEN_ERR(client, nick)            (": 431 " + client + " " + nick + " :No nickname given")
 #define ERRONEUSNICKNAME_ERR(client, nickname)        (": 432 " + client + " " + nickname + " :Erroneus nickname")
 #define NICKNAMEINUSE_ERR(client, nick)                (": 433 * " + client + " " + nick + " :Nickname is already in use")
-#define USERNOTINCHANNEL_ERR(client, nick, channel)    (": 441 " + client + " " + nick + " " + channel + " :They aren't on that channel")
+
 
 #define KEYSET_ERR(channel)                            (": 467 " + channel + " :Channel key already set")	//	a voir si on laisse la possibiliter  de remplacer le mdp.
-#define CHANNELISFULL_ERR(client, channel)            (": 471 " + client + " " + channel + " :Cannot join channel (+l)")
-#define INVITEONLYCHAN_ERR(client, channel)            (": 473 " + client + " " + channel + " :Cannot join channel (+i)")
-#define BADCHANNELKEY_ERR(client, channel)            (": 475 " + client + " " + channel + " :Cannot join channel (+k)")
+
+
+
 */
 
 std::string	skip_words(int n, const std::string &str);
